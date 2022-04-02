3880847464cf8588ed4223477f5d0082786cee76
chaimae bencheikhe <chaaimaeben9@gmail.com>
2022-04-02 20:58:38 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HereComponent } from './components/here/here.component';
+import { HomeComponent } from './components/home/home.component';
+import { DragComponent } from './drag/drag.component';
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent},
+    {
+    path:'here',component:HereComponent
 
-const routes: Routes = [];
+  },
+  {path:"drag",component:DragComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"about",component:AboutComponent}
+
+
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
